d9f02c8e0204a6830d2ffb67652e39468fae6305
carlos.paredes <carlos.paredes@IBLAESBARC00213>
2021-08-11 21:47:21 +0200

    Ajustes mongo

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors())
 //Habilitar express.json / tambien se puede utilizar bodyparser
 app.use(express.json({extended : true}))
 //puerto de la app
-const port = process.env.port || 4000;
+const port = process.env.PORT || 4000;
 //Importar rutas
 app.use('/api/usuarios', require('./routes/usuarios'));
 app.use('/api/auth', require('./routes/auth'))
@@ -19,6 +19,6 @@ app.use('/api/proyectos', require('./routes/proyectos'));
 app.use('/api/tareas', require('./routes/tareas'));
 
 //arrancar server
-app.listen(port,'0.0.0.0', () => {
+app.listen(port,() => {
     console.log (`El servidor esta funcionando en el puerto ${port}`);
 });
